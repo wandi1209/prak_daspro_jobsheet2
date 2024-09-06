47426793c0224ca768ab06c4f8d8a04e219161fe
wandi1209 <wandider@gmail.com>
2024-09-06 19:59:30 +0700

    masukkan biodata wandi

--- prak_daspro_jobsheet2/src/biodata/wandi.java
+++ prak_daspro_jobsheet2/src/biodata/wandi.java
@@ -1,5 +1,17 @@
 package biodata;
 
 public class wandi {
-    
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Wandi");
+        System.out.println("NIM     : 244107020003");
+        System.out.println("Kelas   : 1C");
+        System.out.println("No Absen: 25");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jl. Simpang Remujung No 6A");
+        System.out.println("No HP   : 085250314485");
+        System.out.println("Hobi    : Membaca");
+    }
 }
