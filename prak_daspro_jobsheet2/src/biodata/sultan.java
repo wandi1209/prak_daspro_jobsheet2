70eb6fa0ec45c95e8203f2760d057bb6103b5970
sultanariva <sultannanda55@gmail.com>
2024-09-07 13:06:22 +0700

    Biodata Sultan

--- prak_daspro_jobsheet2/src/biodata/sultan.java
+++ prak_daspro_jobsheet2/src/biodata/sultan.java
@@ -1,5 +1,18 @@
 package biodata;
 
 public class sultan {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Sultan Nashira Ariva");
+        System.out.println("NIM     : 244107020187");
+        System.out.println("Kelas   : 1C");
+        System.out.println("No Absen: 23");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jl. Klampok Kasri 2/183 Malang");
+        System.out.println("No HP   : 0858-5602-1029");
+        System.out.println("Hobi    : Desain Grafis");
+    }
     
 }
